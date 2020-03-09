47646585bfaddd66ed8bcb08813a44f1c02ff989
rafiandria23 <rafiandria23@gmail.com>
2020-03-09 16:56:26 +0700

    Implemented Board Item onChangeText and Apply Button

--- sugoku-client/components/SudokuBoard.jsx
+++ sugoku-client/components/SudokuBoard.jsx
@@ -1,35 +1,36 @@
-import React, {useState, useEffect} from 'react';
-import { TextInput, View, StyleSheet, Button } from 'react-native';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { TextInput, View, StyleSheet, Button } from "react-native";
+import axios from "axios";
 
-const api = axios.create({ baseURL: 'https://sugoku.herokuapp.com' });
+const api = axios.create({ baseURL: "https://sugoku.herokuapp.com" });
 
 const styles = StyleSheet.create({
   boardItem: {
     width: 40,
     height: 40,
-    borderColor: 'black',
+    borderColor: "black",
     borderWidth: 1,
     fontSize: 20,
-    textAlign: 'center'
+    textAlign: "center"
   },
   borderContainer: {
     flex: 1,
-    flexDirection: 'row'
+    flexDirection: "row"
   },
   mainContainer: {
-    flexDirection: 'column',
-    justifyContent: 'center',
-    marginTop: '40%'
+    flexDirection: "column",
+    justifyContent: "center",
+    marginTop: "35%"
   }
 });
 
 export default function SudokuBoard(props) {
-  const { difficulty = 'easy' } = props;
+  const { difficulty = "easy" } = props;
   const [board, setBoard] = useState([]);
 
   useEffect(() => {
-    api.get(`/board?difficulty=${difficulty}`)
+    api
+      .get(`/board?difficulty=${difficulty}`)
       .then(({ data }) => {
         setBoard(data.board);
       })
@@ -39,39 +40,46 @@ export default function SudokuBoard(props) {
   }, []);
 
   const handleNumberChange = (text, coordinate) => {
-    const boardToChange = [...board];
-    boardToChange[coordinate[0]][coordinate[1]] = text;
-    setBoard(boardToChange);
-  }
+    const nums = "0123456789";
+    if (!nums.includes(text)) {
+      alert("Please enter NUMBER!");
+    } else if (text.length > 1) {
+      alert("Please enter ONLY NUMBER BETWEEN 0-9!");
+    } else {
+      const boardToChange = [...board];
+      boardToChange[coordinate[0]][coordinate[1]] = text;
+      setBoard(boardToChange);
+    }
+  };
 
   const renderBoard = () => {
     const boardContainer = board.map((item, conIdx) => {
       const subItems = item.map((subItem, idx) => {
-        return <TextInput onChangeText={(text) => handleNumberChange(text, [conIdx, idx])} key={idx} style={styles.boardItem} value={subItem === 0 ? '' : String(subItem)} />
+        return (
+          <TextInput
+            onChangeText={text => handleNumberChange(text, [conIdx, idx])}
+            key={idx}
+            style={styles.boardItem}
+            value={subItem === 0 ? "" : String(subItem)}
+          />
+        );
       });
-      return (
-        <View>
-          {subItems}
-        </View>
-      );
+      return <View>{subItems}</View>;
     });
-    return (
-      <View style={styles.borderContainer}>
-        {boardContainer}
-      </View>
-    );
+    return <View style={styles.borderContainer}>{boardContainer}</View>;
   };
 
   const validateSudoku = () => {
     const boardToValidate = board.map(item => {
       const subItems = item.map(subItem => {
-        return subItem === '' ? 0 : Number(subItem);
+        return subItem === "" ? 0 : Number(subItem);
       });
       return subItems;
     });
-    api.post('/validate', { board: boardToValidate })
+    api
+      .post("/validate", { board: boardToValidate })
       .then(({ data }) => {
-        console.log(data.status.toUpperCase());
+        alert(data.status.toUpperCase());
       })
       .catch(err => {
         console.log(err.response);
@@ -79,16 +87,15 @@ export default function SudokuBoard(props) {
   };
 
   return (
-  <View style={styles.mainContainer}>
-    {
-      board.length > 0 &&
-      renderBoard()
-      }
-      <Button
-        style={styles.applyButton}
-        title="Apply"
-        onPress={() => validateSudoku()}
-      />
+    <View style={styles.mainContainer}>
+      {board.length > 0 && renderBoard()}
+      <View>
+        <Button
+          style={styles.applyButton}
+          title="Apply"
+          onPress={() => validateSudoku()}
+        />
+      </View>
     </View>
-  )
+  );
 }
