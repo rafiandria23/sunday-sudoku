6515901feee1aa0af87b5f48ecc2546b307fca67
rafiandria23 <rafiandria23@gmail.com>
2020-03-11 15:26:46 +0700

    Implemented Changing Color on background image

--- sugoku-client/src/screens/HomeScreen.jsx
+++ sugoku-client/src/screens/HomeScreen.jsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   Text,
   View,
   TextInput,
   TouchableOpacity,
   StyleSheet,
-  KeyboardAvoidingView
+  KeyboardAvoidingView,
+  Animated
 } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
 
@@ -14,8 +15,34 @@ import styles from "../styles";
 import { setPlayerName } from "../actions/playerActions";
 
 export default ({ navigation, route }) => {
+  const animatedValue = new Animated.Value(0);
   const dispatch = useDispatch();
   const playerName = useSelector(state => state.playerReducer.name);
+
+  const backgroundColorConfig = animatedValue.interpolate({
+    inputRange: [0, 0.2, 0.4, 0.6, 0.8, 1],
+    outputRange: [
+      "#FFEB3B",
+      "#CDDC39",
+      "#009688",
+      "#03A9F4",
+      "#3F51B5",
+      "#FFEB3B"
+    ]
+  });
+
+  const startBackgroundColorAnimation = () => {
+    Animated.loop(
+      Animated.timing(animatedValue, {
+        toValue: 1,
+        duration: 15000
+      })
+    ).start();
+  };
+
+  useEffect(() => {
+    startBackgroundColorAnimation();
+  }, []);
 
   const handleChangeName = name => {
     dispatch(setPlayerName(name));
@@ -30,27 +57,31 @@ export default ({ navigation, route }) => {
   };
 
   return (
-    <KeyboardAvoidingView style={{ flex: 1 }} behavior="padding">
-      <View style={styles.container}>
-        <View style={customStyles.nameContainer}>
-          <Text>Please type your name:</Text>
-          <TextInput
-            style={customStyles.nameField}
-            value={playerName}
-            onChangeText={text => handleChangeName(text)}
-          />
-        </View>
+    <Animated.View
+      style={[styles.centerOnly, { backgroundColor: backgroundColorConfig }]}
+    >
+      <KeyboardAvoidingView style={{ flex: 1 }} behavior="padding">
+        <View style={styles.centerOnly}>
+          <View style={customStyles.nameContainer}>
+            <Text>Please type your name:</Text>
+            <TextInput
+              style={customStyles.nameField}
+              value={playerName}
+              onChangeText={text => handleChangeName(text)}
+            />
+          </View>
 
-        <View>
-          <TouchableOpacity
-            onPress={handleNextButton}
-            style={customStyles.nextButton}
-          >
-            <Text style={customStyles.nextButtonText}>Next!</Text>
-          </TouchableOpacity>
+          <View>
+            <TouchableOpacity
+              onPress={handleNextButton}
+              style={customStyles.nextButton}
+            >
+              <Text style={customStyles.nextButtonText}>Next!</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-      </View>
-    </KeyboardAvoidingView>
+      </KeyboardAvoidingView>
+    </Animated.View>
   );
 };
 
