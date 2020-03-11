8d22b7db8a61e4ae34e20b571fe196328bf5eb28
rafiandria23 <rafiandria23@gmail.com>
2020-03-11 14:21:49 +0700

    Implemented KeyboardAvoidingView on HomeScreen.

--- sugoku-client/src/screens/HomeScreen.jsx
+++ sugoku-client/src/screens/HomeScreen.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
+import React from "react";
 import {
   Text,
   View,
   TextInput,
   TouchableOpacity,
   StyleSheet,
-  Picker
-} from 'react-native';
-import { useDispatch, useSelector } from 'react-redux';
+  KeyboardAvoidingView
+} from "react-native";
+import { useDispatch, useSelector } from "react-redux";
 
-import styles from '../styles';
+import styles from "../styles";
 
-import { setPlayerName } from '../actions/playerActions';
+import { setPlayerName } from "../actions/playerActions";
 
 export default ({ navigation, route }) => {
   const dispatch = useDispatch();
@@ -23,32 +23,34 @@ export default ({ navigation, route }) => {
 
   const handleNextButton = () => {
     if (playerName.length < 1) {
-      alert('Player name cannot be empty!');
+      alert("Player name cannot be empty!");
     } else {
-      navigation.navigate('Difficulty Selection');
+      navigation.navigate("Difficulty Selection");
     }
   };
 
   return (
-    <View style={styles.container}>
-      <View style={customStyles.nameContainer}>
-        <Text>Please type your name:</Text>
-        <TextInput
-          style={customStyles.nameField}
-          value={playerName}
-          onChangeText={text => handleChangeName(text)}
-        />
-      </View>
+    <KeyboardAvoidingView style={{ flex: 1 }} behavior="padding">
+      <View style={styles.container}>
+        <View style={customStyles.nameContainer}>
+          <Text>Please type your name:</Text>
+          <TextInput
+            style={customStyles.nameField}
+            value={playerName}
+            onChangeText={text => handleChangeName(text)}
+          />
+        </View>
 
-      <View>
-        <TouchableOpacity
-          onPress={handleNextButton}
-          style={customStyles.nextButton}
-        >
-          <Text style={customStyles.nextButtonText}>Next!</Text>
-        </TouchableOpacity>
+        <View>
+          <TouchableOpacity
+            onPress={handleNextButton}
+            style={customStyles.nextButton}
+          >
+            <Text style={customStyles.nextButtonText}>Next!</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
+    </KeyboardAvoidingView>
   );
 };
 
@@ -56,29 +58,29 @@ const customStyles = StyleSheet.create({
   nameField: {
     width: 300,
     height: 50,
-    borderColor: 'blue',
+    borderColor: "blue",
     borderWidth: 3,
     fontSize: 20,
-    textAlign: 'center',
+    textAlign: "center",
     margin: 20,
     padding: 8,
     borderRadius: 8
   },
   nextButton: {
-    alignItems: 'center',
-    backgroundColor: 'pink',
+    alignItems: "center",
+    backgroundColor: "pink",
     padding: 10,
     borderRadius: 8,
     width: 200
   },
   nextButtonText: {
     fontSize: 20,
-    fontWeight: 'bold',
-    color: 'white'
+    fontWeight: "bold",
+    color: "white"
   },
   nameContainer: {
-    justifyContent: 'center',
-    alignContent: 'center',
-    alignItems: 'center'
+    justifyContent: "center",
+    alignContent: "center",
+    alignItems: "center"
   }
 });
